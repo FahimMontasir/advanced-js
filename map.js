661b31f8b55522a4aa746ea0fbe9593d9cf1f377
fahim <fahimmontasirshakil@gmail.com>
2021-02-02 14:42:25 +0600

    exploring map

---
+++ map.js
@@ -0,0 +1,5 @@
+const numbers = [1, 4, 5, 7, 9];
+const powerIt = numbers.map(elements => elements*elements);
+console.log(powerIt);
+const doubleIt = numbers.map(x => x* 2);
+console.log(doubleIt);
